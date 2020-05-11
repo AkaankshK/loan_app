95edf630dc1c0ea54e306db1cd55f9bf0a000388
shagilsiddiqui <40637140+shagilsiddiqui@users.noreply.github.com>
2020-05-11 20:21:00 +0530

    Add files via upload

---
+++ lib/PersonalLoan/PersonalLoan.dart
@@ -0,0 +1,58 @@
+var icici={
+  "name":"ICICI",
+  "logo":"assets/personalLoan/icici.png",
+ // "logo2":"assets/BusinessLoan/1a.png",
+  "desc":"Get Attractive Rates on ICICI Bank Personal Loan. Apply Online Now and Get Instant",
+  "amount":"50000-20Lacs",
+  "maxamount":"20Lacs",
+  "tenure":"12-60",
+  "interest":"11.20%",
+  "processing fee":"0",
+  "eligibility":"salary 30000,2 years job experience",
+  "documents":"KYC documents, income proof, passport size photo",
+  "repayment":"eNACH",
+  "earlyrepayment":"5% of outstanding principal amount",
+  "overdue":"Overdue Rule As per company policy",
+  "loandisbursal":"Customer's salary a/c",
+};
+var tata_capital={
+  "name":"TATA CAPITAL",
+  "logo":"assets/personalLoan/17.png",
+  // "logo2":"assets/BusinessLoan/1a.png",
+  "desc":"Get Attractive Rates on ICICI Bank Personal Loan. Apply Online Now and Get Instant",
+  "amount":"1 Lac-25Lacs",
+  "maxamount":"25Lacs",
+  "tenure":"1-6",
+  "interest":"10.99%",
+  "processing fee":"0",
+  "eligibility":"Salary of 15000,1 year of job continuty.",
+  "documents":"Application Form, KYC, Salary Slip and Bank Statement",
+  "repayment":"From the customer's salary account",
+  "earlyrepayment":"Zero charges on Part Pre-Payment",
+  "overdue":"Overdue Rule As per company policy",
+  "loandisbursal":"AS PER THE POLICY",
+
+};
+var Money_view={
+  "name":"",
+  "logo":"assets/personalLoan/17.png",
+  // "logo2":"assets/BusinessLoan/1a.png",
+  "desc":"Get Attractive Rates on ICICI Bank Personal Loan. Apply Online Now and Get Instant",
+  "amount":"1 Lac-25Lacs",
+  "maxamount":"25Lacs",
+  "tenure":"1-6",
+  "interest":"10.99%",
+  "processing fee":"0",
+  "eligibility":"Salary of 15000,1 year of job continuty.",
+  "documents":"Application Form, KYC, Salary Slip and Bank Statement",
+  "repayment":"From the customer's salary account",
+  "earlyrepayment":"Zero charges on Part Pre-Payment",
+  "overdue":"Overdue Rule As per company policy",
+  "loandisbursal":"AS PER THE POLICY",
+
+};
+
+
+var PersonalLoans=[
+  icici,tata_capital,
+];
