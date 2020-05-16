4fc36d5c4053fa13ac5a1670f5aa759fdf2bb93e
akaankshk <akaankshk@gmail.com>
2020-05-16 17:44:50 +0530

    Fixed Overflow Issues.

--- lib/CreditScore/CreditScore.dart
+++ lib/CreditScore/CreditScore.dart
@@ -1,21 +1,18 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-Map<int, Color> colors =
-{
-  50:Color.fromRGBO(136,14,79, .1),
-  100:Color.fromRGBO(136,14,79, .2),
-  200:Color.fromRGBO(136,14,79, .3),
-  300:Color.fromRGBO(136,14,79, .4),
-  400:Color.fromRGBO(136,14,79, .5),
-  500:Color.fromRGBO(136,14,79, .6),
-  600:Color.fromRGBO(136,14,79, .7),
-  700:Color.fromRGBO(136,14,79, .8),
-  800:Color.fromRGBO(136,14,79, .9),
-  900:Color.fromRGBO(136,14,79, 1),
-};
 
+Map<int, Color> colors = {
+  50: Color.fromRGBO(136, 14, 79, .1),
+  100: Color.fromRGBO(136, 14, 79, .2),
+  200: Color.fromRGBO(136, 14, 79, .3),
+  300: Color.fromRGBO(136, 14, 79, .4),
+  400: Color.fromRGBO(136, 14, 79, .5),
+  500: Color.fromRGBO(136, 14, 79, .6),
+  600: Color.fromRGBO(136, 14, 79, .7),
+  700: Color.fromRGBO(136, 14, 79, .8),
+  800: Color.fromRGBO(136, 14, 79, .9),
+  900: Color.fromRGBO(136, 14, 79, 1),
+};
 
 class CreditPage extends StatefulWidget {
   @override
@@ -23,8 +20,8 @@ class CreditPage extends StatefulWidget {
 }
 
 class _CreditPageState extends State<CreditPage> {
-  MaterialColor yellow = MaterialColor(0xffde903b,colors);
-  MaterialColor darkBlue = MaterialColor(0xff0f3f81,colors);
+  MaterialColor yellow = MaterialColor(0xffde903b, colors);
+  MaterialColor darkBlue = MaterialColor(0xff0f3f81, colors);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,9 +33,9 @@ class _CreditPageState extends State<CreditPage> {
     return Stack(
       children: [
         Container(
-          height: MediaQuery.of(context).size.height,
+            height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-            child: Image.asset("assets/background1.png",fit: BoxFit.fill)),
+            child: Image.asset("assets/background1.png", fit: BoxFit.fill)),
         getContent(),
       ],
     );
@@ -46,7 +43,7 @@ class _CreditPageState extends State<CreditPage> {
 
   getContent() {
     return SingleChildScrollView(
-      child:  Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
@@ -54,31 +51,42 @@ class _CreditPageState extends State<CreditPage> {
             children: [
               Padding(
                 padding: const EdgeInsets.only(top: 50),
-                child: Text("Credit Report",style: TextStyle(fontSize: 25),),
+                child: Text(
+                  "Credit Report",
+                  style: TextStyle(fontSize: 25),
+                ),
               )
             ],
           ),
           Row(
             children: [
               Padding(
-                padding: EdgeInsets.only(top: 30,left: 30),
-                child: Text("Here is",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+                padding: EdgeInsets.only(top: 30, left: 30),
+                child: Text(
+                  "Here is",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
               ),
-
             ],
           ),
           Row(
             children: [
               Padding(
                 padding: EdgeInsets.only(left: 30),
-                child: Text("Your ",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+                child: Text(
+                  "Your ",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
               ),
-              Text("Credit Report",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: yellow),)
-
+              Text(
+                "Credit Report",
+                style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold, color: yellow),
+              )
             ],
           ),
           SizedBox(
-            //TODO: Add text names
+              //TODO: Add text names
               height: 200,
               width: 700,
               child: Column(
@@ -88,30 +96,53 @@ class _CreditPageState extends State<CreditPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Image.asset("assets/u.png",height: 90,width: 200,),
-                        Image.asset("assets/uu.png",height: 90,width: 200,),
-
+                        Expanded(
+                            flex: 50,
+                            child: Image.asset(
+                              "assets/u.png",
+                              height: 80,
+                              width: 175,
+                            )),
+                        Expanded(
+                            flex: 50,
+                            child: Image.asset(
+                              "assets/uu.png",
+                              height: 80,
+                              width: 175,
+                            )),
                       ],
                     ),
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Image.asset("assets/uuu.png",height: 90,width: 180,),
-                        Image.asset("assets/uuuu.png",height: 90,width: 200,),
-
+                        Expanded(
+                            flex: 50,
+                            child: Image.asset(
+                              "assets/uuu.png",
+                              height: 80,
+                              width: 175,
+                            )),
+                        Expanded(
+                            flex: 50,
+                            child: Image.asset(
+                              "assets/uuuu.png",
+                              height: 80,
+                              width: 175,
+                            )),
                       ],
                     ),
-                  ]
-
-              )
-          ),
+                  ])),
           Container(
             height: 150,
             width: MediaQuery.of(context).size.width,
             color: darkBlue,
             child: Center(
-              child: Text("Ad for AdMob",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+              child: Text(
+                "Ad for AdMob",
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
             ),
           ),
           Row(
@@ -120,15 +151,39 @@ class _CreditPageState extends State<CreditPage> {
                 padding: EdgeInsets.only(left: 10),
                 child: Image.asset("assets/p.png"),
               ),
-              Text("Get Credit report for ",style: TextStyle(fontSize: 20),),
-              Text("\u20b91500",style: TextStyle(fontSize: 20,decoration: TextDecoration.lineThrough),),
-              Text(" FREE",style: TextStyle(fontSize: 20,color: Colors.red),),
+              Text(
+                "Get Credit report for ",
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                "\u20b91500",
+                style: TextStyle(
+                    fontSize: 20, decoration: TextDecoration.lineThrough),
+              ),
+              Text(
+                " FREE",
+                style: TextStyle(fontSize: 20, color: Colors.red),
+              ),
             ],
           ),
           Row(
             children: [
-              Image.asset("assets/fix-credit-score.png",height: 150,width: 200,),
-              Image.asset("assets/creditscorestuff.jpg",height: 150,width: 200,),
+              Expanded(
+                flex: 50,
+                child: Image.asset(
+                  "assets/fix-credit-score.png",
+                  height: 100,
+                  width: 150,
+                ),
+              ),
+              Expanded(
+                flex:50,
+                child: Image.asset(
+                  "assets/creditscorestuff.jpg",
+                  height: 100,
+                  width: 150,
+                ),
+              ),
             ],
           ),
           Row(
@@ -136,10 +191,12 @@ class _CreditPageState extends State<CreditPage> {
             children: [
               MaterialButton(
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {},
+                child: Text(
+                  "Get it Now!",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
-                onPressed: (){},
-                child: Text("Get it Now!",style: TextStyle(color: Colors.white,fontSize: 20),),
                 color: Colors.green,
                 minWidth: 300,
               )
--- lib/NavigationBar/NavBar.dart
+++ lib/NavigationBar/NavBar.dart
@@ -7,13 +7,28 @@ import 'package:loanapp/CreditScore/CreditScore.dart';
 import 'package:loanapp/HomeScreen/HomeScreen.dart';
 import 'package:loanapp/MoreOptions/MoreOptionsPage.dart';
 import 'package:loanapp/dummy.dart';
-
+Map<int, Color> colors =
+{
+  50:Color.fromRGBO(136,14,79, .1),
+  100:Color.fromRGBO(136,14,79, .2),
+  200:Color.fromRGBO(136,14,79, .3),
+  300:Color.fromRGBO(136,14,79, .4),
+  400:Color.fromRGBO(136,14,79, .5),
+  500:Color.fromRGBO(136,14,79, .6),
+  600:Color.fromRGBO(136,14,79, .7),
+  700:Color.fromRGBO(136,14,79, .8),
+  800:Color.fromRGBO(136,14,79, .9),
+  900:Color.fromRGBO(136,14,79, 1),
+};
 class NavBar extends StatefulWidget {
   @override
   _NavBarState createState() => _NavBarState();
 }
 
 class _NavBarState extends State<NavBar> {
+  MaterialColor freeColor = MaterialColor(0xff01b527,colors);
+  MaterialColor buttonColor = MaterialColor(0xffffa812,colors);
+  MaterialColor lightBlueColor = MaterialColor(0xff3862ff,colors);
   var currentIndex=0;
   final List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
@@ -46,69 +61,82 @@ class _NavBarState extends State<NavBar> {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              MaterialButton(
-                minWidth: 30,
-                onPressed: (){
-                  setState(() {
-                    currentIndex=0;
-                  });
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/money bag icon.png",height: 30,width: 30,),
-                    Text("LoanKwik")
-                  ],
+              Expanded(
+                flex: 20,
+                child: MaterialButton(
+                  onPressed: (){
+                    setState(() {
+                      currentIndex=0;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/money bag icon.png",height: 30,width: 30,color: currentIndex==0?lightBlueColor: Colors.black,),
+                      Text("LoanKwik",style: TextStyle(color:currentIndex==0?lightBlueColor: Colors.black,fontSize: 10),)
+                    ],
+                  ),
                 ),
               ),
-              MaterialButton(
-                minWidth: 30,
-                onPressed: (){
-                  setState(() {
-                    currentIndex=1;
-                  });
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/ol.png",height: 30,width: 30,),
-                    Text("Offer")
-                  ],
+              Expanded(
+                flex: 20,
+                child: MaterialButton(
+                  onPressed: (){
+                    setState(() {
+                      currentIndex=1;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/ol.png",height: 30,width: 30,color: currentIndex==1?lightBlueColor: Colors.black),
+                      Text("Offer",style: TextStyle(color:currentIndex==1?lightBlueColor: Colors.black,fontSize: 10))
+                    ],
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top:20),
-                child: Text("Credit Score"),
+              Expanded(
+                flex: 20,
+                child: Padding(
+                  padding: const EdgeInsets.only(top:20),
+                  child: Text("Credit Score"),
+                ),
               ),
 
-              MaterialButton(
-                minWidth: 30,
-                onPressed: (){
-                  setState(() {
-                    currentIndex=3;
-                  });
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/calcicon.png",height: 30,width: 30,),
-                    Text("Calculator")
-                  ],
+              Expanded(
+                flex: 20,
+                child: MaterialButton(
+
+                  onPressed: (){
+                    setState(() {
+                      currentIndex=3;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/calcicon.png",height: 30,width: 30,color:currentIndex==3?lightBlueColor: Colors.black),
+                      Text("Calculator",style: TextStyle(color:currentIndex==3?lightBlueColor: Colors.black,fontSize: 10))
+                    ],
+                  ),
                 ),
               ),
-              MaterialButton(
-                minWidth: 30,
-                onPressed: (){
-                  setState(() {
-                    currentIndex=4;
-                  });
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/ii.png",height: 30,width: 30,),
-                    Text("More")
-                  ],
+              Expanded(
+                flex: 20,
+                child: MaterialButton(
+
+                  onPressed: (){
+                    setState(() {
+                      currentIndex=4;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/ii.png",height: 30,width: 30,color:currentIndex==4?lightBlueColor: Colors.black),
+                      Text("More",style: TextStyle(color:currentIndex==4?lightBlueColor: Colors.black,fontSize: 10))
+                    ],
+                  ),
                 ),
               ),
             ],
